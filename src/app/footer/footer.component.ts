10a9f4cbe36c149356b102bb38c50d274c220bf8
rifayetazamtalukder <rifayetewucse@gmail.com>
2020-04-07 01:31:41 +0600

    Footer component completed

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  //#region Variables
+  public currentYear: number;
+  //#endregion Variables 
+  // 
   constructor() { }
 
   ngOnInit(): void {
+    this.currentYear = new Date().getFullYear();
   }
 
 }
