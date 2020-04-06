1378d5537ebed233f0a33341ebe3875b2dcf3e90
rifayetazamtalukder <rifayetewucse@gmail.com>
2020-04-06 19:40:33 +0600

    Routing added and app.component.html theke built in text delete kore dichi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HelpPageComponent } from './help-page/help-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'help',
+    component: HelpPageComponent
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  // 
+  // The below wild card component must be at the bottom of the page
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
